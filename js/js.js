c0abf884be8a44b062823311ea708de92b58a512
Chiwaodiego <73324237+Chiwaodiego@users.noreply.github.com>
2021-11-15 08:38:26 -0300

    organizando estilos css en escritorio

--- js/js.js
+++ js/js.js
@@ -5,3 +5,10 @@ btnToggle.addEventListener('click',function(){
 	document.getElementById('menu').classList.toggle('active');
 })
 
+const flip = document.querySelector('.sidebar__buttom');
+
+
+flip.addEventListener('click',function(){
+	document.getElementById('flipicon').classList.toggle('active');
+})
+
