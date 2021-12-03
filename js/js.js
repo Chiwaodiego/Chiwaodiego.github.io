4f298434387c1ad3dc51f9a6b99205f6249f64bb
Chiwaodiego <73324237+Chiwaodiego@users.noreply.github.com>
2021-12-03 08:48:23 -0300

    act

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 const btnToggle = document.querySelector('.sidebar__buttom');
-btnToggle.addEventListener('click',function(){
+btnToggle.addEventListener('click', () => {
 	document.getElementById('menu').classList.toggle('active');
 })
 
 const flip = document.querySelector('.sidebar__buttom');
-flip.addEventListener('click',function(){
+flip.addEventListener('click', () => {
 	document.getElementById('flipicon').classList.toggle('active');
 })
 
